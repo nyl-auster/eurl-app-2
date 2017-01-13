75b95a8aaf58b93388e8df635f890eec538a7730
yann <yann.boisselier@gmail.com>
2017-01-14 00:05:30 +0100

    ajout d'une page 404

---
+++ src/components/pages/PageNotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="aide">
+    <h1>Page non trouvée</h1>
+    <p>La page demandée n'a pas été trouvée </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pageAide'
+}
+</script>
