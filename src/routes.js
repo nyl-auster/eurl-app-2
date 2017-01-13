55589a57e48f766a3029d17df8c433d236e3e865
yann <yann.boisselier@gmail.com>
2017-01-13 15:14:13 +0100

    delete all files to build site

--- src/routes.js
+++
@@ -1,7 +0,0 @@
-import PageAide from 'components/pages/PageAide'
-import PageSimulateur from 'components/pages/PageSimulateur'
-
-export default [
-  { path: '/', component: PageSimulateur },
-  { path: '/aide', component: PageAide }
-]
