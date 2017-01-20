2f379f50e659badf65e74388d64d5b4228e460f8
yann <yann.boisselier@gmail.com>
2017-01-20 13:58:52 +0100

    put toFixedNuber prototype patchnig in its own file

--- src/services/ObjectInterfaces.js
+++ src/services/ObjectInterfaces.js
@@ -1,12 +1,3 @@
-// ajout d'une nouvelle méthode au prototype Number pour arrondir à
-// 2 chiffres après la virgule
-/*
-Number.prototype.toFixedNumber = function(x, base = 2){
-  const pow = Math.pow(base||10,x);
-  return +( Math.round(this*pow) / pow );
-};
-*/
-
 /**
  * Méthodes communes pour nos objets
  * @param datas
