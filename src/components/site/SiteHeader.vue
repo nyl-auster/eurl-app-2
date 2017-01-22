d8da1562c531e93056ea9b3cb96be5ff37ef3414
yann <yann.boisselier@gmail.com>
2017-01-22 10:59:36 +0100

    fix git

--- src/components/site/SiteHeader.vue
+++ src/components/site/SiteHeader.vue
@@ -2,7 +2,7 @@
   <header>
     <div id="site-title"> Simulateur de trésorerie pour EURL</div>
     <div id="site-subtitle">Professions libérales - Impôt sur les sociétés</div>
-    <div id="version">version <strong>ALPHA</strong> - v 0.9.7 - 17/01/2017</div>
+    <div id="version">version <strong>ALPHA</strong> - v 0.9.8 - 22/01/2017</div>
   </header>
 </template>
 
