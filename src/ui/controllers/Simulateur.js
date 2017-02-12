a91cc83d8658e97ef5c96f31c0a9ac759e4bb2c6
yann <yann.boisselier@gmail.com>
2017-02-12 22:03:29 +0100

    using vuex instead of local state

--- src/ui/controllers/Simulateur.js
+++ src/ui/controllers/Simulateur.js
@@ -10,16 +10,8 @@ export default {
     }
   },
   methods: {
-    // mettre à jour les données de notre simulateur
-    setParams: function (formValues) {
-      for (const property in formValues) {
-        this.params[property] = formValues[property];
-      }
-      this.$store.commit('calculatorParams', this.params);
-      this.calculateResults();
-    },
-    calculateResults: function () {
-      this.Results = chargesCalculator(this.$store.state.calculatorParams).getResults();
+    setParams (params) {
+      this.$store.commit('calculatorParams', params);
     }
   }
 }
