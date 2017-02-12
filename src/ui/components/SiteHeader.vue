3ead1e73e21627ee613b9a8d184a108e92411d0b
yann <yann.boisselier@gmail.com>
2017-02-12 12:15:21 +0100

    remove obsolet css

--- src/ui/components/SiteHeader.vue
+++ src/ui/components/SiteHeader.vue
@@ -11,31 +11,3 @@
 
   </header>
 </template>
-
-<style scoped>
-
-  header {
-    text-align:center;
-    margin-top:1rem;
-  }
-
-  #site-title {
-    font-size:1.7rem;
-    text-transform:uppercase;
-  }
-
-  #site-subtitle {
-    font-size:1.2rem;
-    margin-bottom:0.5rem;
-  }
-
-  #version {
-    font-style:italic;
-    font-size:0.9rem;
-    margin:auto;
-
-    margin-bottom:1rem;
-  }
-
-
-</style>
